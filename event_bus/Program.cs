1c306f70dd8f656dff81a9e931a15236f4f609e2
Ahmed Magdy Seleem <Ahmed.Magdy.S.19@gmail.com>
2023-04-04 16:08:36 +0200

    add event_bus service

---
+++ event_bus/Program.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Text.Json;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+var httpClient = new HttpClient();
+
+
+app.MapPost("/events", async httpContext => {
+    
+    var eventReceived = await JsonSerializer.DeserializeAsync<Hashtable>(httpContext.Request.Body);
+    await httpClient.PostAsJsonAsync(new Uri("http://localhost:4000/events"),eventReceived); // for Posts service
+    await httpClient.PostAsJsonAsync(new Uri("http://localhost:4001/events"),eventReceived); // for Comments service
+    await httpClient.PostAsJsonAsync(new Uri("http://localhost:4002/events"),eventReceived); // for Query service
+    httpClient.Dispose();
+
+    await httpContext.Response.WriteAsJsonAsync(new Hashtable{["status"] = "Ok"});
+
+});
+
+
+app.Run();
